3bdde2a53dc35c497af7e5b02450d93352b58f00
frhl <frederik.heymann@gmail.com>
2020-11-25 11:34:43 +0000

    pipelien for processing multiple contrast

--- scripts/03_deexpr.R
+++ scripts/03_deexpr.R
@@ -37,7 +37,7 @@ c(L,M) # quite small library size
 # keep gene rows if there at least 2 experiments (~25%) with at least 200.000 counts
 #keep.exprs <- filterByExpr(y, group=conds)
 #y <- y[keep.exprs,, keep.lib.sizes=FALSE]
-(cpm.threshold <- L/10)
+(cpm.threshold <- M/10)
 keep <- rowSums(cpm(y)> cpm.threshold) >= 10
 table(keep)
 y <- y[keep,]
@@ -88,58 +88,63 @@ plotBCV(y)
 glmfit <- glmFit(y, design)
 
 # look at first differential expression
-H441_SUBvsKO_sub <- glmLRT(glmfit, contrast=cont.matrix[,1])
-de <- decideTestsDGE(H441_SUBvsKO_sub)
+#H441_SUBvsKO_sub <- glmLRT(glmfit, contrast=cont.matrix[,1])
+#de <- decideTestsDGE(H441_SUBvsKO_sub)
 
 # what genes are upregulated / downregulated?
-summary(de <- decideTestsDGE(H441_SUBvsKO_sub, adjust.method = "fdr"))
-detags <- rownames(y)[as.logical(de)]
-(detags_hgnc <- sort(ensembl_to_hgnc(detags)))
+#summary(de <- decideTestsDGE(H441_SUBvsKO_sub, adjust.method = "fdr"))
+#detags <- rownames(y)[as.logical(de)]
+#(detags_hgnc <- sort(ensembl_to_hgnc(detags)))
 
-plot(y$counts[rownames(y$counts) == 'ENSG00000168878',])
-text(y$counts[rownames(y$counts) == 'ENSG00000168878',], labels = colnames(y), cex = 0.4)
+#plot(y$counts[rownames(y$counts) == 'ENSG00000168878',])
+#text(y$counts[rownames(y$counts) == 'ENSG00000168878',], labels = colnames(y), cex = 0.4)
 
-'SFTPB' %in% detags_hgnc # present!
+#'SFTPB' %in% detags_hgnc # present!
 
-plotSmear(H441_SUBvsKO_sub, de.tags=detags, main="H441_SUBvsKO_sub", ylim=c(-10,10))
-abline(h=c(-1,1), col="blue")
+#plotSmear(H441_SUBvsKO_sub, de.tags=detags, main="H441_SUBvsKO_sub", ylim=c(-10,10))
+#abline(h=c(-1,1), col="blue")
 
 # order table by pvalue
-o <- order(H441_SUBvsKO_sub$table$PValue)
-H441_SUBvsKO_sub <- H441_SUBvsKO_sub$table[o,]
+#o <- order(H441_SUBvsKO_sub$table$PValue)
+#H441_SUBvsKO_sub <- H441_SUBvsKO_sub$table[o,]
 
 ## compute and add adjusted p-values
-adjp <- p.adjust(H441_SUBvsKO_sub$PValue, method="fdr")
-H441_SUBvsKO_sub <- cbind(H441_SUBvsKO_sub, adjp)
-H441_SUBvsKO_sub$ensembl_gene_id <- rownames(H441_SUBvsKO_sub)
+#adjp <- p.adjust(H441_SUBvsKO_sub$PValue, method="fdr")
+#H441_SUBvsKO_sub <- cbind(H441_SUBvsKO_sub, adjp)
+#H441_SUBvsKO_sub$ensembl_gene_id <- rownames(H441_SUBvsKO_sub)
 
 # merge mapping
-H441_SUBvsKO_sub <- merge(H441_SUBvsKO_sub, mapping)
-H441_SUBvsKO_sub$significant <- H441_SUBvsKO_sub$adjp < 0.05
-ggplot(H441_SUBvsKO_sub, aes(x = logFC, y = -log10(PValue), color = significant)) +
-  geom_point()
+#H441_SUBvsKO_sub <- merge(H441_SUBvsKO_sub, mapping)
+#H441_SUBvsKO_sub$significant <- H441_SUBvsKO_sub$adjp < 0.05
+#ggplot(H441_SUBvsKO_sub, aes(x = logFC, y = -log10(PValue), color = significant)) +
+#  geom_point()
+
+
 
 
 ### look at different coditions
 
 result <- lapply(1:ncol(cont.matrix), function(i){
   
-  contr <- paste(rownames(cont.matrix)[as.logical(abs(cont.matrix[,i]))], collapse = 'vs')
+  # get contrasts and calc logFC
+  contr_name <- paste(rownames(cont.matrix)[as.logical(abs(cont.matrix[,i]))], collapse = 'vs')
   fit <- glmLRT(glmfit, contrast=cont.matrix[,i])
   de <- decideTestsDGE(fit)
-  
-  # sort by p-value
-  o <- order(fit$table$PValue)
-  fit.tabl <- fit$table[o,]
   
   # get FDR
   FDR <- p.adjust(fit.tabl$PValue, method="fdr")
   fit.tabl <- cbind(fit.tabl, FDR)
   fit.tabl$ensembl_gene_id <- rownames(fit.tabl)
-  fit.tabl$hgnc <- ensembl_to_hgnc(fit.tabl$ensembl_gene_id)
-  #fit.tabl[]
+  final <- merge(fit.tabl, mart, all.x = T)
+  final <- final[,c(7,1,2:6)]
   
-  #mart = getBM(attributes=c('ensembl_gene_id', 'hgnc_symbol'), filters = values = fit.tabl$ensembl_gene_id, mart = ensembl)
+  # sort by FDR
+  o <- order(final$FDR)
+  final <- final[o,]
+  
+  # save to file
+  outpath = paste0('derived/201125_',contr_name,'.csv')
+  write.csv(final, outpath)
   
 })
 
